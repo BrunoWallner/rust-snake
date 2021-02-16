67a20c5a0bdc5f95024d33998b5b80d3f7f77cdf
Luca <b.lucab1211@gmail.com>
2021-02-17 00:34:14 +0100

    reconstructed syntax between main and engine, added color support to canvas and cleared up code

--- src/engine.rs
+++ src/engine.rs
@@ -1,9 +1,10 @@
 #![allow(non_snake_case)]
 use std::io::{self, Write};
+use colored::*;
 
 pub struct Canvas {
     pub size: [i32; 2],
-    pub buffer: Vec<Vec<i32>>,
+    pub buffer: Vec<Vec<i32>>
 }
 
 pub struct Shape {
@@ -12,6 +13,9 @@ pub struct Shape {
 }
 
 impl Canvas {
+    pub fn new(x_size: i32, y_size: i32) -> Self {
+        Canvas {size: [x_size, y_size], buffer: vec![vec![]]}
+    }
     pub fn Init(&mut self) {
         for x in 0..self.size[0] as usize{
             self.buffer.push(Vec::new());
@@ -24,7 +28,7 @@ impl Canvas {
         for y in 0..self.size[1] as usize {
             for x in 0..self.size[0] as usize {
                 if x as u32 == shape.position[0] && y as u32 == shape.position[1] {
-                self.buffer[x][y] = 1;
+                    self.buffer[x][y] = 1;
                 }
             }
         }
@@ -32,7 +36,13 @@ impl Canvas {
     pub fn Update(&self) {
         for y in 0..self.size[1] as usize {
             for x in 0..self.size[0] as usize {
-                print!("{}", self.buffer[x][y]);
+                match self.buffer[x][y] {
+                    0 => print!("  "),
+                    1 => print!("{}", "██".red()),
+                    2 => print!("{}", "██".green()),
+                    3 => print!("{}", "██".blue()),
+                    _ => print!("{}", "?")
+                }
             }
             print!("\n");
             io::stdout().flush().unwrap();
@@ -49,6 +59,9 @@ impl Canvas {
 }
 
 impl Shape {
+    pub fn new(x_pos: u32, y_pos: u32, x_size: u32, y_size: u32) -> Self {
+        Shape {position: [x_pos, y_pos], size: [x_size, y_size]}
+    }
     pub fn Move(&mut self, direction: [u32; 2]) {
         self.position[0] += direction[0];
         self.position[1] += direction[1];
--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,17 @@ mod functions;
 use functions::sleep;
 
 fn main() {
-    let mut canvas = Canvas {size: [16, 8], buffer: vec![vec![0]]};
+    let mut canvas = Canvas::new(32, 32);
 
-    let mut shape = Shape {size: [1, 1], position: [3, 1]};
+    let mut shape = Shape::new(1, 1, 1, 1);
 
     canvas.Init();
 
-    'running: loop {
+    loop {
         canvas.Clear();
 
         canvas.Draw(&shape);
-        shape.Move([1, 0]);
-
-        //debug
-        println!("{} : {}", shape.position[0], shape.position[1]);
+        shape.Move([1, 1]);
 
         canvas.Update();
 
